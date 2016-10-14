b28a476373c4f00a09f230ca60eb5f5b023fcd78
Elizabeth Linn <elise_81@yahoo.com>
2016-10-13 22:40:01 -0700

    add console event handler

--- automation-tools/main.js
+++ automation-tools/main.js
@@ -1,13 +1,21 @@
 (function () {
     'use strict';
 
+
     var casper,
         tests,
-        testIndex = 1,
-        message = 'OMG';
+        testIndex = 1;
+
 
     // Bring the Casper
     casper = require('casper').create(); // Why we have to run the create immediately?
+
+
+    // Event handler for console logs in the remote page context
+    casper.on('remote.message', function (msg) {
+        console.log('remote message: ' + msg);
+    });
+
 
     // Create our tests
     tests = [{
@@ -21,15 +29,14 @@
     }];
 
 
+
     // console.log(tests[testIndex].name, tests[testIndex].url, tests[testIndex].screenshotOutputLocation);
 
     // Get the page title plus our fancy message
     casper.start(tests[testIndex].url, function () {
-        var title = this.evaluate(function (message) {
-            return message + ' ' + document.title;
-        }, message);
-
-        casper.echo('Page title is ' + title);
+        this.evaluate(function () {
+            console.log(document.title);
+        });
         // this.capture(tests[testIndex].screenshotOutputLocation);
     });
 
