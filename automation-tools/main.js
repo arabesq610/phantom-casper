209320f73b367e65a60694168ddac8a9ea6a8844
Elizabeth Linn <elise_81@yahoo.com>
2016-10-17 22:11:03 -0700

    capture screenshots at different resolutions using each

--- automation-tools/main.js
+++ automation-tools/main.js
@@ -1,119 +1,36 @@
-/*global window, JSON*/
-(function () {
-    'use strict';
+var casper = require('casper').create({
+    verbose: true
+});
+var urls = ['http://www.google.ca', 'http://www.bing.ca', 'http://packtpub.com'];
+var viewportSizes = [480, 720, 1200]; // widths
+var counter = 0;
+
+// console.log(urls);
+console.log(viewportSizes);
 
 
-    var casper,
-        fs,
-        evalObj,
-        tests,
-        testIndex = 1,
-        casperSettingsObj = null;
-    /* { // One option is to override the UA, but this won't get around detecation of headless browsers (i.e., PhantomJS, or 'Mozilla/5.0 (Macintosh; Intel Mac OS X) AppleWebKit/538.1 (KHTML, like Gecko) CasperJS/1.1.0-beta5+PhantomJS/2.1.1 Safari/538.1')
-        pageSettings: {
-            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
-        }
-    } */
+casper
+    .start()
+    .repeat(viewportSizes.length, function () {
+        'use strict';
+        var viewportSize = viewportSizes[counter];
 
+        casper
+            .viewport(viewportSize, 1000)
+            .each(urls, function (self, item, index) {
+                self.thenOpen(item, function () {
+                    var title = this.getTitle();
+                    console.log(title);
 
-    // Bring the Casper
-    casper = require('casper').create(casperSettingsObj); // Why we have to run the create immediately? Not sure.
-
-
-    // Bring filesystem
-    fs = require('fs');
-
-
-    // Event handler for console logs in the remote page context
-    casper.on('remote.message', function (msg) {
-        console.log('remote message: ' + msg);
-    });
-
-
-    // Create our tests
-    tests = [{
-        name: 'google',
-        url: 'https://www.google.com/',
-        screenshotOutputLocation: './output/google.com.png'
-    }, {
-        name: 'google',
-        url: 'http://www.google.ca/',
-        screenshotOutputLocation: './output/google.ca.png'
-    }, {
-        name: 'eliselinn',
-        url: 'http://www.eliselinn.com/',
-        screenshotOutputLocation: './output/eliselinn.png'
-    }, {
-        name: 'packtpub',
-        url: 'https://www.packtpub.com/packt/offers/free-learning',
-        screenshotOutputLocation: './output/packtpub.png'
-    }];
-
-    // Some info about the remote page we're about to dive into
-    // console.log(tests[testIndex].name, tests[testIndex].url, tests[testIndex].screenshotOutputLocation);
-
-
-    // Output the page title
-    casper.start(tests[testIndex].url, function () {
-        this.fill('form', {
-            q: 'omfg'
-        }, true);
-    });
-    casper.wait(1000, function () {
-        // Note `this` refers to the casper object inside the remote page context
-
-        this.capture(tests[testIndex].screenshotOutputLocation);
-
-
-        evalObj = this.evaluate(function () {
-            var i,
-                returnElement,
-                targetElements = document.querySelectorAll('.g h3 a'),
-                data = [],
-                e,
-                link,
-                title;
-            for (i = 0; i < targetElements.length; i += 1) {
-                e = targetElements[i];
-
-                link = e.getAttribute('href');
-                title = e.text;
-                returnElement = {
-                    link: link,
-                    title: title
-                };
-                data.push(returnElement);
-            }
-            return data;
-
-
-
-            // var username,
-            //     password;
-
-            // username = document.getElementById('email');
-            // password = document.getElementById('password');
-
-            // This allows us to see the UA which we can use to emulate in chrome and see the page as it's rendered to us while running as PhantomJS
-            // console.log(window.navigator.userAgent);
-
-            // Return something useful
-            // return {
-            //     username: username,
-            //     password: password
-            // };
-        });
-
-        // console.log(evalObj);
-
+                    this.wait(2000, function () {
+                        this.capture('./images/screenshot_' + index + '_' + viewportSize + '.png');
+                    });
+                });
+            });
+        counter += 1;
 
     });
 
+casper.run();
 
-
-    casper.run(function () {
-        fs.write('./output/data.json', JSON.stringify(evalObj, null, '\t'));
-        this.exit();
-    });
-
-}());
+// casper.exit();
